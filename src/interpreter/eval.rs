08ed5a1ab6d923b119cf4fc67d83b44e4f380ead
over <franciscosebastianyjunior@hotmail.com>
2025-08-23 17:29:01 -0300

    Implementar interpretação de Var e VarDecl

--- src/interpreter/eval.rs
+++ src/interpreter/eval.rs
@@ -1,11 +1,13 @@
+use std::collections::HashMap;
+
 use crate::{lexer::value::Value, parser::expr::Expr};
 
-pub fn eval(expr: &Expr) -> Result<Value, String> {
+pub fn eval(expr: &Expr, env: &mut HashMap<String, Value>) -> Result<Value, String> {
     match expr {
         Expr::Literal(v) => Ok(v.clone()),
         Expr::Var { name } => {
-            // TODO! Lookup variable in environment.
-            Ok(Value::String("variable_value".to_string()))
+            let value = env.get(name).ok_or(format!("Undefined variable: {}", name))?;
+            Ok(value.clone())
         }
         Expr::FieldGet { name, field } => {
             // TODO! Lookup variable in environment and get field.
@@ -18,7 +20,7 @@ pub fn eval(expr: &Expr) -> Result<Value, String> {
         Expr::ComponentCons { name, fields } => {
             let evaluated_fields = fields
                 .iter()
-                .map(|(f_name, f_expr)| eval(f_expr).map(|v| (f_name.clone(), v)))
+                .map(|(f_name, f_expr)| eval(f_expr, env).map(|v| (f_name.clone(), v)))
                 .collect::<Result<Vec<_>, _>>()?;
 
             Ok(Value::Component {
