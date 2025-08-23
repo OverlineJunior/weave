222f40bbbca576772b3606b63baaf07b418b6440
over <franciscosebastianyjunior@hotmail.com>
2025-08-23 18:48:29 -0300

    Renomear Value::Component para ComponentInst

--- src/interpreter/eval.rs
+++ src/interpreter/eval.rs
@@ -6,25 +6,26 @@ pub fn eval(expr: &Expr, env: &mut HashMap<String, Value>) -> Result<Value, Stri
     match expr {
         Expr::Literal(v) => Ok(v.clone()),
         Expr::Var { name } => {
-            let value = env.get(name).ok_or(format!("Undefined variable: {}", name))?;
+            let value = env
+                .get(name)
+                .ok_or(format!("Undefined variable: {}", name))?;
             Ok(value.clone())
         }
         Expr::FieldGet { name, field } => {
             // TODO! Lookup variable in environment and get field.
             Ok(Value::String("field_value".to_string()))
         }
-		Expr::EntityCons(exprs) => {
-			// TODO! Evaluate expressions and construct entity with id.
-			Ok(Value::Entity(0))
-		}
+        Expr::EntityCons(exprs) => {
+            // TODO! Evaluate expressions and construct entity with id.
+            Ok(Value::Entity(0))
+        }
         Expr::ComponentCons { name, fields } => {
             let evaluated_fields = fields
                 .iter()
                 .map(|(f_name, f_expr)| eval(f_expr, env).map(|v| (f_name.clone(), v)))
                 .collect::<Result<Vec<_>, _>>()?;
 
-            Ok(Value::Component {
-                name: name.clone(),
+            Ok(Value::ComponentInst {
                 fields: evaluated_fields,
             })
         }
